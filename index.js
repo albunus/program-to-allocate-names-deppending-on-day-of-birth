6e42831f1519a4ee02d3e631ac5bd349452b1c0d
albunus <albunus.nyalita@student.moringaschool.com>
2021-09-07 13:00:47 +0300

    it will change on how to get birth date on js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 function generateName() {
     var dates = document.getElementById('date').value;
-    var date =parseInt(dates);
     var month = document.getElementById('month').value;
-    var yearOfBirth = document.getElementById('year').value;
-    var century = yearOfBirth.slice(0, 2);
-    var year = yearOfBirth.slice(2, 4);
+    var year=document.getElementById('year').value;
+    var yearOfBirth = new Date(year+ '/' +month + '/' + dates);
+    var dayOfBirth = yearOfBirth.getDay();
     var genders = document.getElementById("gender");
     var gender = genders.options[genders.selectedIndex].text;
     var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
     var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var dayOfBirths = (((century /4) -2 *century- 1) + ((5 * year / 4)) + ((26 * (month + 1)/ 10)));
-    var day = ((dayOfBirths)+(date))%7; 
-    var dayOfBirth = Math.floor(day);
     if (gender === "Male") {
         document.getElementById("result").innerHTML ="Amazing!" +"You were born on " + dayOfWeek[dayOfBirth] + " and your name is " + maleName[dayOfBirth ];
     } else if(gender === "Female") {
         document.getElementById("result").innerHTML ="Amazing!" +"You were born on " + dayOfWeek[dayOfBirth ] + " and your name is " + femaleName[dayOfBirth ];
-    }
+    };
+    function myFunction() {
+        document.getElementById("myForm").reset();
+      }
 }
